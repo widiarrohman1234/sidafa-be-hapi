426358aded7a1c9e9b961bcf1d915dd8d8a69864
Widi Arrohman <widiarrohman1234@gmail.com>
2023-06-07 05:34:13 +0700

    scripts and appspec.yml

---
+++ script/application_start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DIR="$HOME/sidafa-be-hapi"
+cd $DIR
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+npm install
+
+node ./index.js > app.out.log 2> app.err.log < /dev/null &
---
+++ script/before_install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# install nvm dan node
+echo "CEK NVM"
+if [ ! command -v nvm &> /dev/null ]; then
+  echo "NVM TIDAK DITEMUKAN"
+  echo "NVM AKAN DIINSTALL"
+
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+  . ~/.nvm/nvm.sh
+  echo "NVM BERHASIL DIINSTALL"
+  nvm install 16
+  node -e "console.log('NODEJS ' + process.version + ' BERHASIL DIINSTALL')"
+fi
+
+# CEK DIREKTORI
+DIR="/home/ec2-user/sidafa-be-hapi"
+echo "CEK DIRECTORY $DIR"
+if [ -d "$DIR" ]; then
+  echo "$DIR TELAH ADA"
+else
+  echo "$DIR BELUM ADA"
+  echo "MEMBUAT DIRECTORY"
+  mkdir ${DIR}
+  sudo chmod -R 777 "$DIR"
+fi
